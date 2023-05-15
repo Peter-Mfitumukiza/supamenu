48be9685f9759aa298ee9d0ae66a078095ca1df7
Mfitumukiza Peter <61537587+Peter-Mfitumukiza@users.noreply.github.com>
2023-05-15 15:13:59 +0200

    ft: welcome sreen

--- src/Screens/WelcomeScreen.js
+++ src/Screens/WelcomeScreen.js
@@ -3,13 +3,23 @@ import { StyleSheet, View, Text } from "react-native";
 export default function WelcomeScreen() {
     return(
         <View style={styles.container}>
-            <Text style={styles.welcome}>SupaMenu</Text>
+            <Text style={ styles.welcomeText } >Supa<Text style={ styles.white }>Menu</Text></Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: ""
+        backgroundColor: "#F7941D",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    welcomeText:{
+        fontSize: 35,
+        fontWeight: "bold"
+    },
+    white:{
+        color: "white"
     }
 })
